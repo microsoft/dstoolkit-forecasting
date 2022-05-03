bb3ab1660b471a2d03d1b3a87c017804818bf19b
M.M.B <98582541+mmbellani@users.noreply.github.com>
2022-05-03 11:23:39 +0200

    adjustment to plots

--- Code/Plotting/plots.py
+++ Code/Plotting/plots.py
@@ -15,39 +15,13 @@ from matplotlib import pyplot as plt
 import matplotlib.dates as mdates
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
+from adjustText import adjust_text
 
 # custom functions
 from Configuration.config import cfg_path
 from Code.Utils.utils import Utils
 
 class Plots:
-    def get_text_positions(x_data, y_data, txt_width, txt_height):
-        a = zip(y_data, x_data)
-        text_positions = y_data.copy()
-        for index, (y, x) in enumerate(a):
-            local_text_positions = [i for i in a if i[0] > (y - txt_height) 
-                                and (abs(i[1] - x) < txt_width * 2) and i != (y,x)]
-            if local_text_positions:
-                sorted_ltp = sorted(local_text_positions)
-                if abs(sorted_ltp[0][0] - y) < txt_height: #True == collision
-                    differ = np.diff(sorted_ltp, axis=0)
-                    a[index] = (sorted_ltp[-1][0] + txt_height, a[index][1])
-                    text_positions[index] = sorted_ltp[-1][0] + txt_height
-                    for k, (j, m) in enumerate(differ):
-                        #j is the vertical distance between words
-                        if j > txt_height * 2: #if True then room to fit a word in
-                            a[index] = (sorted_ltp[k][0] + txt_height, a[index][1])
-                            text_positions[index] = sorted_ltp[k][0] + txt_height
-                            break
-        return text_positions
-
-    def text_plotter(x_data, y_data, text_positions, axis,txt_width,txt_height):
-        for x,y,t in zip(x_data, y_data, text_positions):
-            axis.text(x - txt_width, 1.01*t, '%d'%int(y),rotation=0, color='blue')
-            if y != t:
-                axis.arrow(x, t,0,y-t, color='red',alpha=0.3, width=txt_width*0.1, 
-                        head_width=txt_width, head_length=txt_height*0.5, 
-                        zorder=0,length_includes_head=True)
                 
     def sliding_line_plot(df, serie_to_plot, id, i, chart_title=""):
         """
